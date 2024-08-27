bd7b1069606d5e810a162973a05e9874358f30fe
jailcomfranssa <jail_son_0@hotmail.com>
2024-08-27 02:05:33 -0300

    add controller new pages

--- src/main/java/com/scm/scm20/controllers/PageController.java
+++ src/main/java/com/scm/scm20/controllers/PageController.java
@@ -2,7 +2,9 @@ package com.scm.scm20.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PageController {
@@ -24,5 +26,23 @@ public class PageController {
     public String servicesPage(){
         System.out.println("services page handler");
         return "services";
+    }
+
+    @GetMapping("/contact")
+    public String contactPage(){
+        System.out.println("contact page handler");
+        return new String("contact");
+    }
+
+    @GetMapping("/login")
+    public String loginPage(){
+        System.out.println("login page handler");
+        return new String("login");
+    }
+
+    @GetMapping("/register")
+    public String registerPage(){
+        System.out.println("register page handler");
+        return new String("register");
     }
 }
